4e2753278fb371e2ffac5f5d331f5a76a473c766
朝林摆渡 <gxp461869141@163.com>
2018-12-11 19:48:50 +0800

    commit

--- src/main/java/com/xcn/springboottest/Sample.java
+++ src/main/java/com/xcn/springboottest/Sample.java
@@ -1,22 +1,41 @@
 package com.xcn.springboottest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableAutoConfiguration
-public class Sample {
-	
-	@RequestMapping("/")
-	public String home(){
-		return "hello world";
-	}
-	
+import java.io.PrintStream;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sample.class,args);
-	}
+@RestController
+//@ComponentScan(value="com.xcn.springboottest")
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class Sample {
+    private Logger logger = LoggerFactory.getLogger(Sample.class);
+
+    @RequestMapping("/")
+    public String home() {
+        logger.info(this.getClass().getName());
+        return "hello world";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Sample.class);
+        Banner banner = new Banner() {
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+
+            }
+        };
+//		app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
 
 }
